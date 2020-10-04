a83f74d010d2da1c8cb4bd9d9c585188e473c6b9
Gaurank Sharma <gaurank124@gmail.com>
2020-10-04 10:47:51 +0530

    create a test home container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import "./App.css";
+import HomeContainer from "./containers/HomeContainer";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <HomeContainer />
+  </div>;
 }
 
 export default App;
---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HomeContainer() {
+  return (
+    <div className="HomeContainer">
+      <p>Test Home Container</p>
+    </div>
+  );
+}
