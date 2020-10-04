fbce4003d7cae7c0866c32875ae555422dad3185
Gaurank Sharma <gaurank124@gmail.com>
2020-10-04 11:40:07 +0530

    create ui for right side component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,22 +3,25 @@ import React from "react";
 export default class Home extends React.Component {
   render() {
     return (
-     <div style={{ background: "#ba26ff", height: "100vh"}}>
+     <div style={{ background: "#8E15F6", height: "100vh"}}>
        <div style={{background: "#fff", display:'flex', justifyContent:'space-between', alignItems:'center', padding:'30ox 300px'}}>
-     <div style={{ color: '#ba26ff', fontSize: 20, fontWeight: 700}}>
+     <div style={{ color: '#8E15F6', fontSize: 20, fontWeight: 700}}>
          LOGO
      </div>
      <div style={{display:'flex', alignItems:'center'}}>
             <div style={{ fontSize: 18, margin: "0 12px" }}>Home</div>
             <div style={{ fontSize: 18, margin: "0 12px" }}>My Portfolio</div>
             <div style={{ fontSize: 18, margin: "0 12px" }}>Clients</div>
-            <div style={{ fontSize: 18, margin: "0 12px" }}>Get In Touch</div>
+            <div style={{ fontSize: 18, margin: "0 12px"}}><button>Get In Touch</button></div>
      </div>
      </div>
      {/* Card Content */}
      <div style={{margin:"50px 300px 0", display:'flex', justifyContent:'space-between'}}>
       <div style={{flex: 0.48, height:'80vh', background:'white', overflow:'scroll', borderRadius: 5, padding:'10px'}}>
        
+      </div>
+      <div style={{flex:0.48, height:"80vh", background:"white", overflow:"scroll",borderRadius: 5, padding:"10px"}}>
+
       </div>
      </div>
      </div>
