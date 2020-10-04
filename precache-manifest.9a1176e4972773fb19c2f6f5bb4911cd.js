78eb50684ee7b9d643bfa8d1ecc2c1bb1ceedfe9
Gaurank Sharma <gaurank124@gmail.com>
2020-10-04 14:09:10 +0530

    Updates

---
+++ precache-manifest.9a1176e4972773fb19c2f6f5bb4911cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea40fe46cc03afbace088bd16308706",
+    "url": "/react-task/index.html"
+  },
+  {
+    "revision": "5f034ef85c2e5eb87626",
+    "url": "/react-task/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7545cefa90bd237b2b25",
+    "url": "/react-task/static/js/2.0a033c72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-task/static/js/2.0a033c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f034ef85c2e5eb87626",
+    "url": "/react-task/static/js/main.7f51e770.chunk.js"
+  },
+  {
+    "revision": "56107e0356ea2c57f29d",
+    "url": "/react-task/static/js/runtime-main.5f3dd1ff.js"
+  }
+]);
